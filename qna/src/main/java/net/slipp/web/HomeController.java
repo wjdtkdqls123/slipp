2476b2c78a44b531465f7b8ab87a1f6524913010
SAMSUNG <SAMSUNG@LAPTOP-MJVSU4LA>
2020-01-07 11:57:51 +0900

    spring mvc setting, controller spring setting

---
+++ qna/src/main/java/net/slipp/web/HomeController.java
@@ -0,0 +1,13 @@
+package net.slipp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String Home() {
+		// home.jsp
+		return "home";
+	}
+}
